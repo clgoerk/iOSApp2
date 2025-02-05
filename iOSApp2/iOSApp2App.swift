6fad6b6ba7cc0f236685b94f2bb4e6523021882e
clgoerk <Christopherlee.Goerk@triosstudent.com>
2025-02-05 13:31:02 -0500

    Initial Commit

---
+++ iOSApp2/iOSApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp2App.swift
+//  iOSApp2
+//
+//  Created by Chris Goerk on 2025-02-05.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
